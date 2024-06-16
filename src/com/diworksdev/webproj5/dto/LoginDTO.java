6396ecc75dfe685aca3ac427b0a1cbbfc43ad8bb
sakitamako <sa.919.1025.ki@gmail.com>
2024-06-16 14:53:48 +0900

    フレームワーク５、P22

---
+++ src/com/diworksdev/webproj5/dto/LoginDTO.java
@@ -0,0 +1,37 @@
+package com.diworksdev.webproj5.dto;
+
+public class LoginDTO {
+
+	//テーブルから取得するデータに対応したフィールド変数を宣言
+	private String username;
+	private String password;
+
+	//フィールド変数に対応したgetterとsetterを定義
+	//Actionクラスから呼び出され、usernameフィールドの値をActionに渡す
+	public String getUsername() {
+		return username;
+
+	}
+
+	//フィールド変数に対応したgetterとsetterを定義
+	//DAOクラスから呼び出され、引数として受け取ったテーブルの値を自身のusernameフィールドに格納
+	public void setUsername(String username) {
+		this.username = username;
+
+	}
+
+	//フィールド変数に対応したgetterとsetterを定義
+	//Actionクラスから呼び出され、passwordフィールドの値をActionに渡す
+	public String getPassword() {
+		return password;
+
+	}
+
+	//フィールド変数に対応したgetterとsetterを定義
+	//DAOクラスから呼び出され、引数として受け取ったテーブルの値を自身のpasswordフィールドに格納
+	public void setPassword(String password) {
+		this.password = password;
+
+	}
+
+}
