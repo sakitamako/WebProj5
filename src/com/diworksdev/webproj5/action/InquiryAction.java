292083ad2dafeeeb9702893f4708bad47c406fdb
sakitamako <sa.919.1025.ki@gmail.com>
2024-06-16 16:06:25 +0900

    フレームワーク５、P31

---
+++ src/com/diworksdev/webproj5/action/InquiryAction.java
@@ -0,0 +1,17 @@
+package com.diworksdev.webproj5.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+//struts2が持つActionSupportというクラスを継承
+//（Actionクラスは基本的にこのクラスを継承）
+//LoginAciton（子クラス） extends（継承） ActionSupport（親クラス）
+//すでにあるクラスとにたクラスを作る場合、元のクラスに必要な機能だけを追加する形で、新しいクラスを作ることを継承
+public class InquiryAction extends ActionSupport {
+
+	//管理コマンド・メッセージをコマンド・サーバーに送信し、何らかの応答メッセージを待ちます
+	public String execute() {
+		return SUCCESS;
+
+	}
+
+}
